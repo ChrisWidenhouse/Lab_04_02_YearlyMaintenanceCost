1aa81724c7abd3056223b733cd6353e68ae9e58a
ChrisWidenhouse <159856391+ChrisWidenhouse@users.noreply.github.com>
2024-02-12 19:55:15 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,20 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        double springCost = 150.32;
+        double summerCost = 450.50;
+        double winterCost = 895.50;
+        double fallCost = 3.50;
+        double yearlyCost = 0;
+
+        System.out.println("Spring cost is " + springCost);
+        System.out.println("Summer cost is " + summerCost);
+        System.out.println("Winter cost is " + winterCost);
+        System.out.println("Fall cost is " + fallCost);
+
+        yearlyCost = springCost + summerCost + winterCost + fallCost;
+
+        System.out.println("Yearly cost is " + yearlyCost);
+    }
+}
